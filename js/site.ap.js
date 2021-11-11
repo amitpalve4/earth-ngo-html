3e7636fd7cefed266b3cd1a8c663dfccff82fe73
amitpalve <amit.palve4@gmail.com>
2021-11-11 16:35:29 +0530

    Homepage Complete

--- js/site.ap.js
+++ js/site.ap.js
@@ -12,6 +12,16 @@ $(document).ready(function(){
    
   });
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50){  
+        $('.headerMob').addClass("sticky");
+    }
+    else{
+        $('.headerMob').removeClass("sticky");
+    }
+   
+  });
+
   if($('.ap__banner__slider').length > 0){
     $('.ap__banner__slider').slick({
       slidesToShow: 1,
@@ -36,7 +46,25 @@ $(document).ready(function(){
       speed: 1000,
       autoplay: true,
       autoplaySpeed: 1500,
+      responsive: [
 
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            
+          }
+        },
+        
+        ]
     });
   }
 
@@ -62,9 +90,66 @@ $(document).ready(function(){
       speed: 1000,
       autoplay: true,
       autoplaySpeed: 1500,
+      responsive: [
 
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            
+          }
+        },
+        
+        ]
     });
   }
+
+  var $toggleButton = $('.toggle-button'),
+	$menuWrap = $('.menu-wrap'),
+	$sidebarArrow = $('.sidebar-menu-arrow');
+
+	$toggleButton.on('click', function() {
+	$(this).toggleClass('button-open');
+	$menuWrap.toggleClass('menu-show');
+	});
+  
+  $sidebarArrow.click(function() {
+	$(this).next().slideToggle(300);
+	});
+
+  $('.mainDrop > a').on("click", function() {
+    
+    if($(this).hasClass('active')){
+
+      $(this).removeClass('active');
+      $(this).siblings('.ap__dropMenu').hide();  
+
+    }else{
+      $('.mainDrop > a').removeClass('active');
+      $(this).addClass("active");
+      $(".ap__dropMenu").hide();
+      $(this).siblings(".ap__dropMenu").show();
+    }
+    
+    return false; 
+  });
+
+  $('.popup-youtube').magnificPopup({
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
 
 });
 
